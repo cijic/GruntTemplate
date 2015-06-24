03b84d5e0ffba375e873e119cb9985975119945e
cijic <altcode@ya.ru>
2015-06-24 20:24:06 +0300

    Gruntfile: * Added ftp-deploy usage; * Added CSS/js minimization; * Added directories if not exist; * Changes in settings;

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,29 +1,42 @@
-module.exports = function(grunt) {
-
-    var assetsFolder = 'assets/';
-    var releaseFolder = 'release/';
+module.exports = function(grunt)
+{
+    var assetsFolder  = '../../www/laravel5/public/assets/';
+    var releaseFolder = '../../www/laravel5/public/assets_min/';
 
     // 1. Вся настройка находится здесь
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
-
         concat: {
-            dist: {
+            js: {
                 src: [
-                    assetsFolder + 'js/libs/*.js',
-                    assetsFolder + 'js/dev/script.js'
+                    releaseFolder + 'js/' + 'jquery.js',
+                    releaseFolder + 'js/' + 'bootstrap.min.js',
+                    releaseFolder + 'js/' + 'jssor.js',
+                    releaseFolder + 'js/' + 'jssor.slider.js',
+                    releaseFolder + 'js/' + 'internal.js'
                 ],
-                dest: releaseFolder + 'js/production.min.js',
+                dest: releaseFolder + 'js/production.js',
+            },
+            css : {
+                src: [
+                    releaseFolder + 'css/bootstrap.css',
+//                    releaseFolder + 'css/app.css',
+//                    releaseFolder + 'css/admin.css',
+                    releaseFolder + 'css/style.css'
+                ],
+                dest: releaseFolder + 'css/production.css'
             }
         },
-
         uglify: {
-            build: {
-                src:  releaseFolder + 'js/production.min.js',
-                dest: releaseFolder + 'js/production.min.js'
+            my_target: {
+                files: [{
+                    expand: true,
+                    cwd: assetsFolder + 'js',
+                    src: '**/*.js',
+                    dest: releaseFolder + 'js'
+                }]
             }
         },
-
         imagemin: {
             dynamic: {
                 files: [{
@@ -34,16 +47,45 @@ module.exports = function(grunt) {
                 }]
             }
         },
-
         cssmin: {
+            options: {
+                keepSpecialComments : 0
+            },
             target: {
                 files: [{
                     expand: true,
                     cwd: assetsFolder + 'css',
-                    src: ['*.css', '!*.min.css'],
-                    dest: releaseFolder + 'css',
-                    ext: '.min.css'
+                    src: ['*.css'],
+                    dest: releaseFolder + 'css'
                 }]
+            }
+        },
+        'ftp-deploy': {
+            build: {
+                auth: {
+                    host: 'change_it',
+                    port: 21,
+                    authKey: 'change_it'
+                },
+                src: releaseFolder,
+                dest: '/httpdocs/laravel5/public/assets_min/',
+                exclusions: ['path/to/source/folder/**/.DS_Store', 'path/to/source/folder/**/Thumbs.db', 'path/to/dist/tmp']
+            }
+        },
+        mkdir: {
+            all: {
+                options: {
+                    create: [releaseFolder]
+                },
+            },
+        },
+        watch: {
+            scripts: {
+                files: [assetsFolder + 'js/*.js', assetsFolder + 'css/*.css'],
+                tasks: ['mkdir', 'uglify', 'cssmin', 'concat'],
+                options: {
+                    spawn: false,
+                },
             }
         }
     });
@@ -53,7 +95,11 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-ftp-deploy');
+    grunt.loadNpmTasks('grunt-mkdir');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-concat-css');
 
     // 4. Указываем, какие задачи выполняются, когда мы вводим «grunt» в терминале
-    grunt.registerTask('default', ['concat', 'uglify', 'imagemin', 'cssmin']);
+    grunt.registerTask('default', ['mkdir', 'uglify', 'cssmin', 'concat', 'imagemin']);
 };
